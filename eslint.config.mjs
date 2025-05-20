20dfd173e5c3804bd726bb5e717d7bb001d297bc
Rohit1608 <rohitg1882001@gmail.com>
2025-05-20 18:00:27 +0530

    Update eslint.config.mjs with FlatCompat for Next.js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+// eslint.config.mjs
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -9,6 +10,8 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+];
 
 export default eslintConfig;
